29142844e18478b69868d14ebffd8d84824d31a1
ramdisi <ramdisiabey@gmail.com>
2025-08-20 23:09:16 +0530

    add:data insertion logic

--- src/main/java/controller/NewMarkSheetController.java
+++ src/main/java/controller/NewMarkSheetController.java
@@ -1,10 +1,15 @@
 package controller;
-
+import db.DB;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class NewMarkSheetController {
 
@@ -22,7 +27,28 @@ public class NewMarkSheetController {
 
     @FXML
     void btn_createMarkSheet_OnClick(ActionEvent event) {
-
+        Connection con = DB.getConnection();
+        String examNo;
+        try {
+            ResultSet resultSet = con.prepareStatement("select examNo from exam order by examNo desc limit 1").executeQuery();
+            if (resultSet.next()){
+                examNo = String.format("E%03d",Integer.parseInt(resultSet.getString("examNo").substring(1))+1);
+            }else{
+                examNo="E001";
+            }
+            String [] subjectArray = txt_subjectNames.getText().split(",");
+            for (String subject : subjectArray){
+                PreparedStatement preparedStatement = con.prepareStatement("insert into exam values(?,?,?,?,?)");
+                preparedStatement.setString(1,examNo);
+                preparedStatement.setObject(2,date_dateOfExam.getValue());
+                preparedStatement.setString(3,subject);
+                preparedStatement.setString(4,txt_teacherName.getText());
+                preparedStatement.setString(5,txt_examTitle.getText());
+                preparedStatement.execute();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- src/main/java/db/DB.java
+++ src/main/java/db/DB.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DB {
-    private Connection conn;
+    private static Connection conn;
     private DB(){
         //avoid instance generating
     }
-    public Connection getConnection (){
+    public static Connection getConnection (){
         if (conn == null){
             try {
-                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mark-report-finalizer");
+                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mark_report_finalizer","root","1234");
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
